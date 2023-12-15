57875628eaa82b758b1e02c4954c80c21f54407c
Tribhuwan <mathincosmos@gmail.com>
2023-12-15 18:13:35 +0530

    implement client side validation using zodResolver

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,30 @@
 "use client";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
+import { createIssueSchema } from "@/app/validationSchema";
 import axios from "axios";
 import { useState } from "react";
-interface IssueForm {
-  title: String;
-  description: String;
-}
+import { z } from "zod";
+
+// infer type using zod
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssue = () => {
   const router = useRouter();
   const [error, setError] = useState("");
 
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   return (
     <div className="max-w-xl">
       {error && (
@@ -34,16 +43,31 @@ const NewIssue = () => {
         })}
         className=" space-y-3"
       >
-        <TextField.Root className="max-w-full">
-          <TextField.Input {...register("title")} placeholder="Title" />
-        </TextField.Root>
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
+        <div>
+          <TextField.Root className="max-w-full">
+            <TextField.Input {...register("title")} placeholder="Title" />
+          </TextField.Root>
+          {errors.title && (
+            <Text color="red" as="p">
+              {errors.title.message}
+            </Text>
           )}
-        />
+        </div>
+        <div>
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <SimpleMDE placeholder="Description" {...field} />
+            )}
+          />
+          {errors.description && (
+            <Text color="red" as="p">
+              {errors.description.message}
+            </Text>
+          )}
+        </div>
+
         <Button>Submit New Issue</Button>
       </form>
     </div>
---
+++ app/validationSchema.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+// create schema validator
+export const createIssueSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255),
+  description: z.string().min(1, "Description is required"),
+});
