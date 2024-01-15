aa89b084505e9f6360a0dcce62b9abbb94d07ba7
Tribhuwan <mathincosmos@gmail.com>
2024-01-15 12:19:53 +0530

    refactor navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -16,64 +16,79 @@ import {
 } from "@radix-ui/themes";
 
 const NavBar = () => {
+  return (
+    <nav className=" py-6 space-x-6  mb-5 px-5 border-b">
+      <Container>
+        <Flex direction="row" align="center" justify="between">
+          <NavLinks />
+          <Authoptions />
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+const NavLinks = () => {
   const currentPath = usePathname();
   const links = [
     { href: "/", label: "Dashboard" },
     { href: "/issues/list", label: "Issues" },
   ];
-
-  const { status, data: session } = useSession();
-
   return (
-    <nav className=" py-6 space-x-6  mb-5 px-5 border-b">
-      <Container>
-        <Flex direction="row" align="center" justify="between">
-          <Box>
-            <Flex gap="4" align="center">
-              <Link href="/">
-                <AiFillBug />
+    <Box>
+      <Flex gap="4" align="center">
+        <Link href="/">
+          <AiFillBug />
+        </Link>
+        <ul className="flex space-x-6">
+          {links.map((link) => (
+            <li key={link.href}>
+              <Link
+                className={classnames({
+                  navlink: true,
+                  "!text-zinc-900": currentPath === link.href,
+                })}
+                href={link.href}
+              >
+                {link.label}
               </Link>
-              <ul className="flex space-x-6">
-                {links.map((link) => (
-                  <li key={link.href}>
-                    <Link
-                      className={classnames({
-                        "text-zinc-800": currentPath == link.href,
-                        "text-zinc-500": currentPath !== link.href,
-                        "hover:text-zinc-800 transition-colors": true,
-                      })}
-                      href={link.href}
-                    >
-                      {link.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Flex>
-          </Box>
-          <Box>
-            {status == "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger className="cursor-pointer">
-                  <Avatar radius="full" size="3" src={session.user!.image!} fallback="" />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <Text>{session.user!.email}</Text>
-                  <Button color="red" variant="soft">
-                    <Link href="/api/auth/signout">Logout</Link>
-                  </Button>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status == "unauthenticated" && (
-              <Button color="blue" variant="soft">
-                <Link href="/api/auth/signin">Login</Link>
-              </Button>
-            )}
-          </Box>
-        </Flex>
-      </Container>
-    </nav>
+            </li>
+          ))}
+        </ul>
+      </Flex>
+    </Box>
   );
+};
+
+const Authoptions = () => {
+  const { status, data: session } = useSession();
+  if (status == "loading") return null;
+  if (status == "unauthenticated")
+    return (
+      <Button color="blue" variant="soft">
+        <Link href="/api/auth/signin">Login</Link>
+      </Button>
+    );
+  else
+    return (
+      <Box>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger className="cursor-pointer">
+            <Avatar
+              radius="full"
+              size="2"
+              src={session!.user!.image!}
+              fallback="?"
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <Text>{session!.user!.email}</Text>
+            <Button color="red" variant="soft">
+              <Link href="/api/auth/signout">Logout</Link>
+            </Button>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </Box>
+    );
 };
 export default NavBar;
