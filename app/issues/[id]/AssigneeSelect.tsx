bf13f529199739dcdd5dc1484d831e820131670f
Tribhuwan <mathincosmos@gmail.com>
2024-01-16 17:10:00 +0530

    create custom hook

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,23 +1,24 @@
 "use client";
 
+import { Skeleton } from "@/app/components";
 import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Skeleton } from "@/app/components";
 import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    retry: 3,
-    staleTime: 60 * 1000,
-  });
+  const { data: users, error, isLoading } = useUsers();
+  function assignIssue(userId: string) {
+    axios
+      .patch("/api/issues/" + issue.id, {
+        assignedToUserId: userId || null,
+      })
+      .then(
+        () => toast.success("Change Assignee"),
+        () => toast.error("Changes couldn't be saved")
+      );
+  }
 
   if (error) return null;
   if (isLoading) return <Skeleton />;
@@ -25,16 +26,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || "null"}
-        onValueChange={(userId) => {
-          axios
-            .patch("/api/issues/" + issue.id, {
-              assignedToUserId: userId || null,
-            })
-            .then(
-              (res) => toast.success("Change Assignee"),
-              (err) => toast.error("Changes couldn't be saved")
-            );
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
@@ -53,4 +45,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () =>
+  useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    retry: 3,
+    staleTime: 60 * 1000,
+  });
+
 export default AssigneeSelect;
