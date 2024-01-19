9c1284a6db3a97f97d31aa60d9ef70160c8e052e
Tribhuwan <mathincosmos@gmail.com>
2024-01-19 12:10:54 +0530

    add IssueSummary

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import IssueSummary from "./IssueSumary";
 import LatestIssue from "./LatestIssue";
 
-export default function Home() {
-  return <LatestIssue />;
+export default async function Home() {
+  const closed = await prisma?.issue.count({
+    where: {
+      status: "CLOSED",
+    },
+  });
+  const inProgress = await prisma?.issue.count({
+    where: {
+      status: "IN_PROGRESS",
+    },
+  });
+  const open = await prisma?.issue.count({
+    where: {
+      status: "OPEN",
+    },
+  });
+  // return <LatestIssue />;
+  return (
+    <IssueSummary closed={closed!} open={open!} inProgress={inProgress!} />
+  );
 }
